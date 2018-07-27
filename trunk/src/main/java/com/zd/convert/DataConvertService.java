eb085ba502ad629a320505dd1b904b011c5834a1
duanjt1314 <479224006@qq.com>
2018-07-27 09:30:08 +0800

    缓存写文件和版本号查看
    1.从kafka获取数据并写入文件，修改为每3分钟写入一次，每个文件最多5000条数据
    2.增加版本号查看功能

--- trunk/src/main/java/com/zd/convert/DataConvertService.java
+++ trunk/src/main/java/com/zd/convert/DataConvertService.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import com.zd.util.LogHelper;
 
 public class DataConvertService {
+	private static String VERSION = "1.2.0";
 
 	/**
 	 * 程序启动入口,默认配置文件需在jar包同级
@@ -15,19 +16,25 @@ public class DataConvertService {
 	 */
 	public static void main(String[] args) {
 		try {
+			if (args.length == 1) {
+				if (args[0].equals("-v")) {
+					System.out.println("Version:" + VERSION);
+				}
+				return;
+			}
 			StartUp startUp = new StartUp();
 			LogHelper.getLogger().info("转换程序启动中...");
 			startUp.Start();
 			// 添加程序结束监听 ，用于释放系统资源
 			Runtime.getRuntime().addShutdownHook(new Thread() {
 				public void run() {
-					startUp.Stop();//系统结束
+					startUp.Stop();// 系统结束
 				}
 			});
-			
+
 			LogHelper.getLogger().info("转换程序启动成功");
 		} catch (Exception ex) {
-			LogHelper.getLogger().error("转换程序出现未识别的异常", ex);			
+			LogHelper.getLogger().error("转换程序出现未识别的异常", ex);
 		}
 	}
 
