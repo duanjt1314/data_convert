58c217f6a7864e40dc860ab753bde3f8d02461a0
duanjt1314 <479224006@qq.com>
2018-11-23 13:41:41 +0800

    kafka的groupId修改、kafka中title读取方式修改
    作者:段江涛
    版本:1.4.4
    时间:2018-11-23
    修改内容:
    	1.修改读取topic列头的方式，之前是从表config_datasource中获取，现在修改为先从数据库读取，如果没有读取到就从
    		config/topic-list.properties中读取
    	2.将kafka的groupId由之前的zdkafka修改为data_convert
    部署说明:
    	1.如果用到该功能，请在config文件夹下面增加topic-list.properties文件。
    		文件内容可用通过脚本：select CONCAT(`code`,'=',columns) from
    bdplatform.config_datasource where type='TOPIC'获取
    		文件的格式内容是topic=json串	
    备注:
    	1.
    --------------------------------------------------------------------

--- trunk/src/main/java/com/zd/util/TableTitle.java
+++ trunk/src/main/java/com/zd/util/TableTitle.java
@@ -1,21 +1,50 @@
 package com.zd.util;
 
+import java.io.File;
 import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import com.zd.config.SystemConfig;
 
 import cn.zdsoft.common.model.DataTable;
 
 public class TableTitle {
 	private static DataTable table;
+	private static Config config;
 
 	static {
-		String sql = "select code,columns from config_datasource where type='TOPIC' ";
-		table = new DBAction(SystemConfig.DataBase).getData(sql);
+		try {
+			String sql = "select code,columns from config_datasource1 where type='TOPIC' ";
+			table = new DBAction(SystemConfig.DataBase).getData(sql);
+
+			if (table == null || table.size() == 0) {
+				String msg = "查询表config_datasource数据失败,将从config/topic-list.properties下读取数据\r\n";
+				msg += "如果没有该文件,可创建该文件并通过脚本:select CONCAT(`code`,'=',columns) from bdplatform.config_datasource where type='TOPIC'获取数据";
+				LogHelper.logger.warn(msg);
+
+				Resource resource = new ClassPathResource("config" + File.separator + "topic-list.properties");
+				if (resource.exists()) {
+					Config _config = ConfigFactory.parseFile(resource.getFile());
+					config = _config;
+					LogHelper.logger.info("找到并加载了文件:" + resource.getURI());
+				} else {
+					LogHelper.logger.error("未找到文件:" + resource.getURI());
+				}
+
+			}
+		} catch (Exception ex) {
+			LogHelper.logger.error("", ex);
+		}
 	}
 
 	/**
@@ -24,22 +53,26 @@ public class TableTitle {
 	 * @return
 	 */
 	public static String getTitle(String topic) {
-		for (int i = 0; i < table.size(); i++) {
-			if (table.get(i).get("code").equals(topic)) {
-				String cls = table.get(i).get("columns").toString();
-				Type type = new TypeToken<List<Map>>() {
-				}.getType();
-				List<Map> list = new Gson().fromJson(cls, type);
+		if (config != null) {
+			return config.getString(topic);
+		} else {
+			for (int i = 0; i < table.size(); i++) {
+				if (table.get(i).get("code").equals(topic)) {
+					String cls = table.get(i).get("columns").toString();
+					Type type = new TypeToken<List<Map>>() {
+					}.getType();
+					List<Map> list = new Gson().fromJson(cls, type);
 
-				String result = "";
-				for (Map map : list) {
-					if (result.equals("")) {
-						result += map.get("column").toString();
-					} else {
-						result += "\t" + map.get("column").toString();
+					String result = "";
+					for (Map map : list) {
+						if (result.equals("")) {
+							result += map.get("column").toString();
+						} else {
+							result += "\t" + map.get("column").toString();
+						}
 					}
+					return result;
 				}
-				return result;
 			}
 		}
 		return "";
