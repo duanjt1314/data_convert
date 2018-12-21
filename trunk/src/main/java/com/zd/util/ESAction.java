a152e081ba55f8c3451ed3da3a8651b18548bc50
duanjt1314 <479224006@qq.com>
2018-12-21 19:01:52 +0800

    1.访问ES，增加判断，如果帐号为空，就不插入帐号密码，从而支持没有安装x-pack的情况。 2.修改x-pack-transport的版本，之前5.1版本改错了

--- trunk/src/main/java/com/zd/util/ESAction.java
+++ trunk/src/main/java/com/zd/util/ESAction.java
@@ -12,29 +12,31 @@ import com.zd.config.ElasticSearch;
 import com.zd.config.ElasticSearch.Host;
 import com.zd.config.ElasticSearch.Setting;
 
+import cn.zdsoft.common.StringUtil;
+
 public class ESAction {
-	ElasticSearch elasticSearch=null;
+	ElasticSearch elasticSearch = null;
+
 	public ESAction(ElasticSearch elasticSearch) {
 		this.elasticSearch = elasticSearch;
 	}
-	
+
 	/**
 	 * 获得ES的连接
 	 * @return
 	 * @throws UnknownHostException
 	 */
-	public TransportClient getClient() throws UnknownHostException{
-		//设置
-		org.elasticsearch.common.settings.Settings.Builder builder= Settings.builder();
+	public TransportClient getClient() throws UnknownHostException {
+		// 设置
+		org.elasticsearch.common.settings.Settings.Builder builder = Settings.builder();
 		for (Setting setting : elasticSearch.getSettings()) {
-			builder.put(setting.getKey(),setting.getVal());
+			builder.put(setting.getKey(), setting.getVal());
 		}
-		//帐号密码
-		builder.put("xpack.security.user", elasticSearch.getUser()+":"+elasticSearch.getPassword());
-		Settings setting =builder.build();//构造settings
-		
+		builder.put("xpack.security.user", elasticSearch.getUser() + ":" + elasticSearch.getPassword());
+		Settings setting = builder.build();// 构造settings
+
 		TransportClient client = new PreBuiltXPackTransportClient(setting);
-		
+
 		for (Host host : elasticSearch.getHosts()) {
 			InetSocketTransportAddress address = //
 					new InetSocketTransportAddress(InetAddress.getByName(host.getIp()), host.getPort());
